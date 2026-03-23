cb7d509586d0fe0279a4d256c3c69051c2280580
Hoyoung Park <hoyoungpark.ds@gmail.com>
2026-03-23 16:56:47 +0900

    [Silver III] Title: 바이러스, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -0,0 +1,34 @@
+import sys
+N = int(input())
+M = int(input())
+
+adj = [[] for _ in range(N)]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    adj[a-1].append(b-1)
+    adj[b-1].append(a-1)
+
+check= [0] * N
+check[0] = 1
+
+while True:
+    new = False
+    for i in range(N):
+        if check[i] == 0:
+            continue
+
+        for j in adj[i]:
+            if check[j] == 0:
+                check[j] = 1
+
+                new = True
+    if not new:
+        break
+
+answer = 0
+for i in range(1,N):
+    if check[i] == 1:
+        answer += 1
+
+print(answer)

1e0156fb4c8c7ea79125e652ef52995a47223538
Hoyoung Park <hoyoungpark.ds@gmail.com>
2026-04-10 14:57:32 +0900

    [Silver III] Title: Four Squares, Time: 136 ms, Memory: 111324 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,22 +1,15 @@
-import math
+import sys
+input = sys.stdin.readline
+
 N = int(input())
+dp = [0] + [4] * N  
 
-# 1개 체크
-if int(math.sqrt(N))**2 == N:
-    print(1)
-    exit()
+for i in range(1, N + 1):
+    if (i ** 0.5).is_integer():
+        dp[i] = 1
+        continue
 
-# 2개 체크
-for i in range(1, int(math.sqrt(N)) + 1):
-    if int(math.sqrt(N - i*i))**2 == N - i*i:
-        print(2)
-        exit()
+    for j in range(1, int(i**0.5)+1):  
+        dp[i] = min(dp[i], 1 + dp[i - j*j])
 
-# 3개 or 4개 체크 (Legendre)
-n = N
-while n % 4 == 0:
-    n //= 4
-if n % 8 == 7:
-    print(4)
-else:
-    print(3)
+print(dp[N])
